f79ee1031d7ca2322bf48224d99c5369707fc7d9
Jakub Zalas <jakub@zalas.pl>
2021-09-02 13:23:28 +0100

    Migrate rabbitmq tests to testcontainers
    Signed-off-by: Jakub Zalas <jakub@zalas.pl>

--- rmqbounce.sh
+++
@@ -1,4 +0,0 @@
-# bounce the docker volume xoom-lattice-exchange-rabbitmq in the docker-compose.yml
-docker-compose -p "dev" down
-docker volume rm xoom-lattice-exchange-rabbitmq
-docker-compose -p "dev" up -d
---
+++ src/test/java/io/vlingo/xoom/lattice/exchange/rabbitmq/testcontainers/SharedRabbitMQContainer.java
@@ -0,0 +1,28 @@
+package io.vlingo.xoom.lattice.exchange.rabbitmq.testcontainers;
+
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class SharedRabbitMQContainer extends RabbitMQContainer {
+  private static final String IMAGE_NAME = "rabbitmq:3.8";
+  private static SharedRabbitMQContainer instance;
+
+  private SharedRabbitMQContainer() {
+    super(DockerImageName.parse(IMAGE_NAME));
+  }
+
+  @SuppressWarnings("resource")
+  public static SharedRabbitMQContainer instance() {
+    if (instance == null) {
+      instance = (SharedRabbitMQContainer) new SharedRabbitMQContainer()
+              .withExposedPorts(4369, 5672, 25672, 15672);
+      instance.start();
+    }
+    return instance;
+  }
+
+  @Override
+  public void stop() {
+    // do nothing, the JVM handles shut down
+  }
+}
